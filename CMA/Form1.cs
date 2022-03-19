be6bf4e8d9777dbb46dc87a97904700b588f7e12
Rus <definitelyrus@gmail.com>
2022-03-19 16:00:32 +0800

    Added functioning Purchase & Reset buttons, applied bugfix.

--- CMA/Form1.cs
+++ CMA/Form1.cs
@@ -32,182 +32,202 @@ namespace CMA
 
             InitializeComponent();
         }
+        private void MainFormShown(object sender, EventArgs e)
+        {
+
+        }
         #endregion
 
         #region Column 1
         private void ChangedCounter11(object sender, EventArgs e)
         {
-            a2c(11, menuCounter11.Value);
+            A2C(11, menuCounter11.Value);
         }
 
         private void ChangedCounter12(object sender, EventArgs e)
         {
-            a2c(12, menuCounter11.Value);
+            A2C(12, menuCounter12.Value);
         }
 
         private void ChangedCounter13(object sender, EventArgs e)
         {
-            a2c(13, menuCounter11.Value);
+            A2C(13, menuCounter13.Value);
         }
 
         private void ChangedCounter14(object sender, EventArgs e)
         {
-            a2c(14, menuCounter11.Value);
+            A2C(14, menuCounter14.Value);
         }
         #endregion
 
         #region Column 2
         private void ChangedCounter21(object sender, EventArgs e)
         {
-            a2c(21, menuCounter11.Value);
+            A2C(21, menuCounter21.Value);
         }
 
         private void ChangedCounter22(object sender, EventArgs e)
         {
-            a2c(22, menuCounter11.Value);
+            A2C(22, menuCounter22.Value);
         }
 
         private void ChangedCounter23(object sender, EventArgs e)
         {
-            a2c(23, menuCounter11.Value);
+            A2C(23, menuCounter23.Value);
         }
 
         private void ChangedCounter24(object sender, EventArgs e)
         {
-            a2c(24, menuCounter11.Value);
+            A2C(24, menuCounter24.Value);
         }
         #endregion
 
         #region Column 3
         private void ChangedCounter31(object sender, EventArgs e)
         {
-            a2c(31, menuCounter11.Value);
+            A2C(31, menuCounter31.Value);
         }
 
         private void ChangedCounter32(object sender, EventArgs e)
         {
-            a2c(32, menuCounter11.Value);
+            A2C(32, menuCounter32.Value);
         }
 
         private void ChangedCounter33(object sender, EventArgs e)
         {
-            a2c(33, menuCounter11.Value);
+            A2C(33, menuCounter33.Value);
         }
 
         private void ChangedCounter34(object sender, EventArgs e)
         {
-            a2c(34, menuCounter11.Value);
+            A2C(34, menuCounter34.Value);
         }
         #endregion
 
         #region Column 4
         private void ChangedCounter41(object sender, EventArgs e)
         {
-            a2c(41, menuCounter11.Value);
+            A2C(41, menuCounter41.Value);
         }
 
         private void ChangedCounter42(object sender, EventArgs e)
         {
-            a2c(42, menuCounter11.Value);
+            A2C(42, menuCounter42.Value);
         }
 
         private void ChangedCounter43(object sender, EventArgs e)
         {
-            a2c(43, menuCounter11.Value);
+            A2C(43, menuCounter43.Value);
         }
 
         private void ChangedCounter44(object sender, EventArgs e)
         {
-            a2c(44, menuCounter11.Value);
+            A2C(44, menuCounter44.Value);
         }
         #endregion
 
         #region Clickables
         private void ClickedReset(object sender, EventArgs e)
         {
+            PriceLabel.Text = "Php0";
+            cart.Clear();
 
+            menuCounter11.Value = 0;
+            menuCounter12.Value = 0;
+            menuCounter13.Value = 0;
+            menuCounter14.Value = 0;
+            menuCounter21.Value = 0;
+            menuCounter22.Value = 0;
+            menuCounter23.Value = 0;
+            menuCounter24.Value = 0;
+            menuCounter31.Value = 0;
+            menuCounter32.Value = 0;
+            menuCounter33.Value = 0;
+            menuCounter34.Value = 0;
+            menuCounter41.Value = 0;
+            menuCounter42.Value = 0;
+            menuCounter43.Value = 0;
+            menuCounter44.Value = 0;
+
+            PurchaseButton.Enabled = false;
+            ResetButton.Enabled = false;
         }
 
         private void ClickedPurchase(object sender, EventArgs e)
         {
+            Dictionary<MenuItem, int> itemCountContainer = new();
+            string outputMsg = "Receipt:\n";
+            float total = 0;
 
+            foreach (MenuItem item in cart)
+            {
+                if (itemCountContainer.ContainsKey(item)) itemCountContainer[item] += 1;
+                else itemCountContainer.Add(item, 1);
+            }
+
+            foreach (KeyValuePair<MenuItem, int> dict in itemCountContainer)
+            {
+                outputMsg += "\n"+ dict.Value + "x " + dict.Key.ItemName + " - Php" + dict.Key.Price;
+                total += dict.Key.Price * dict.Value;
+            }
+
+            outputMsg += "\n\nTotal: Php" + total;
+
+            MessageBox.Show(outputMsg, "Purchase Successful!");
+            ClickedReset(sender, e);
         }
         #endregion
-
-        private void MainFormShown(object sender, EventArgs e)
-        {
-            
-        }
 
         #region Shortcuts
         /// <summary>
         /// Automatically adds/removes items from the internal cart.
         /// </summary>
-        private void a2c(int idInt, decimal count)
+        private void A2C(int idInt, decimal count)
         {
-            Console.WriteLine("\nSetting count for " + idInt + " to " + count + ".");
+            Console.WriteLine("Setting count for " + idInt + " to " + count + ".");
+
+            LinkedList<MenuItem> cartCopy = new(cart);
+            string IdStr = idInt.ToString();
             int countInt = Convert.ToInt16(count);
-            string id = idInt.ToString();
-            bool print = true;
             decimal totalPrice = 0;
 
-            //Copies the contents of the internal cart. It will crash if you modify a list while using it as an index.
-            LinkedList<MenuItem> tempCart = new(cart);
+            cart.Clear();
 
-            //For each MenuItem in cart...
-            foreach (MenuItem cartItem in tempCart)
+            int resetCounter = 0;
+            foreach (MenuItem item in cartCopy)
             {
-                //If item's ID is equal to input ID...
-                if (cartItem.ID == id)
+                if (item.ID != IdStr)
                 {
-                    //Remove item from cart.
-                    cart.Remove(cartItem);
+                    cart.AddLast(item);
+                    totalPrice += Convert.ToDecimal(item.Price);
                 }
-
-                //One-time print.
-                if (print)
-                {
-                    Console.WriteLine("Removed " + cartItem.ItemName + " from the cart.");
-                    print = false;
-                }
-
-                //This will empty the cart of the target item.
-                //They will be added back if count is more than 0.
+                else resetCounter++;
             }
-
-            //Copies changes back to internal cart.
-            cart = new(tempCart);
-
-            Console.WriteLine("Cart Length: " + cart.Count);
-
-            //If count is more than 0...
-            if (countInt > 0)
+            Console.WriteLine("Removed " + IdStr + " " + resetCounter + " times.\n");
+            
+            resetCounter = 0;
+            for (int i = 0; i < countInt; i++)
             {
-                //For each MenuItem in menuList...
-                foreach (MenuItem item in menuList)
-                {
-                    //If item's ID is equal to input ID...
-                    if (item.ID == id)
-                    {
-                        //For X times...
-                        for (int X = 0; X < countInt; X++)
-                        {
-                            Console.WriteLine("Added " + item.ItemName + " to cart " + (X+1) + " time(s).");
+                Console.WriteLine("Index: " + i + "/" + countInt);
+                if (count <= 0) break;
 
-                            //Add MenuItem to cart.
-                            cart.AddLast(item);
-                        }
+                Console.WriteLine("Adding to list...");
+                foreach (MenuItem menuItem in menuList)
+                {
+                    if (menuItem.ID == IdStr)
+                    {
+                        cart.AddLast(menuItem);
+                        totalPrice += Convert.ToDecimal(menuItem.Price);
                     }
                 }
             }
 
-            foreach (MenuItem item in cart)
-            {
-                totalPrice += Convert.ToDecimal(item.Price);
-                Console.WriteLine("Price: " + item.Price + "\nTotal: " + totalPrice);
-            }
+            Console.WriteLine("Cart has " + cart.Count + " items.");
+            foreach (MenuItem item in cart) Console.Write(item.ID + " ");
+            PriceLabel.Text = "Php" + totalPrice.ToString();
 
-            PriceLabel.Text = "Php" + totalPrice;
+            ResetButton.Enabled = true;
+            PurchaseButton.Enabled = true;
         }
         #endregion
     }
